efad27c0b50a4b5222b313c7d1a58e636983bc35
LiLinqing <148536831+Wonqinglong@users.noreply.github.com>
2024-04-05 04:17:30 +0800

    Update sketch.js

--- Genetic algorithm/sketch.js
+++ Genetic algorithm/sketch.js
@@ -9,7 +9,7 @@ var bestEver;
 var statusP;
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(1200, 740);
     var order = [];
     for (var i = 0; i < totalCities; i++) {
         var v = createVector(random(width), random(height)); // Fixed typo: changed random to random(height)
@@ -23,29 +23,36 @@ function setup() {
 
     statusP = createP('').style('font-size', '32px'); // Fixed typo: changed 32p to 32px
 }
-
+function calculateFitness() {
+    for (var i = 0; i < population.length; i++) {
+        var d = calcDistance(cities, population[i]);
+        fitness[i] = 1 / (d + 1);
+    }
+}
 function draw() {
+    findBest(); 
+
     background(0);
 
     // GA
     calculateFitness();
     normalizeFitness();
     nextGeneration();
-
-    for (var i = 0; i < population.length; i++) {
-        var d = calcDistance(cities, population[i]); // Added missing arguments cities and population[i]
+    if (bestEver) {
+      
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        beginShape();
+        for (var i = 0; i < bestEver.length; i++) {
+            var n = bestEver[i];
+            vertex(cities[n].x, cities[n].y);
+            ellipse(cities[n].x, cities[n].y, 16, 16);
+        }
+        endShape();
     }
 
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    beginShape();
-    for (var i = 0; i < bestEver.length; i++) {
-        var n = bestEver[i];
-        vertex(cities[n].x, cities[n].y);
-        ellipse(cities[n].x, cities[n].y, 16, 16);
-    }
-    endShape();
+   
 }
 
 function swap(a, i, j) {
